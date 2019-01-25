a4a7f36b2b84550822a3c66c994123a15a8426b8
Nico Rinck <nicorinck1995@gmail.com>
2019-01-25 11:49:06 +0100

    add normalization to TrialDataTransformation

--- src/main/java/preprocess_data/TrialDataTransformation.java
+++ src/main/java/preprocess_data/TrialDataTransformation.java
@@ -11,19 +11,29 @@ public class TrialDataTransformation {
 
     private final FrameDataManipulationStrategy manipulator;
     private final FrameConverter converter;
+    private final FrameNormalizationStrategy normalizationStrategy;
 
     //defines how a frame of marker-data is converted to a list of writables
-    public TrialDataTransformation(FrameLabelingStrategy frameLabelingStrategy, FrameDataManipulationStrategy manipulator) {
+    public TrialDataTransformation(FrameLabelingStrategy frameLabelingStrategy,
+                                   FrameDataManipulationStrategy manipulator,
+                                   FrameNormalizationStrategy normalizationStrategy) {
         this.converter = new FrameConverter(frameLabelingStrategy);
         this.manipulator = manipulator;
+        this.normalizationStrategy = normalizationStrategy;
     }
 
     public TrialDataTransformation(FrameLabelingStrategy frameLabelingStrategy) {
-        this.converter = new FrameConverter(frameLabelingStrategy);
-        manipulator = null;
+        this(frameLabelingStrategy,null,null);
     }
 
-    ArrayList<ArrayList<Writable>> transformFrameData(Frame frame) {
+    ArrayList<ArrayList<Writable>> transformFrameData(final Frame frame) {
+        if (normalizationStrategy != null) {
+            return convertFrameData(normalizationStrategy.normalizeMarker(frame));
+        }
+        return convertFrameData(frame);
+    }
+
+    private ArrayList<ArrayList<Writable>> convertFrameData(final Frame frame) {
         if (manipulator != null) {
             return converter.convertFramesToListOfWritables(manipulator.manipulateFrame(frame));
         }
